94a2b2e8cfaf3ec38eb9d63791af534bd4ae118b
Rohith <cdrohithm@gmail.com>
2022-02-27 11:02:47 +0530

    Add Sort

---
+++ src/components/Sort/Sort.jsx
@@ -0,0 +1,23 @@
+const BubbleSort = ({ array, setArray, i, j, setI, setJ }) => {
+    let dataArray = [...array];
+    const size = array.length;
+    let swap = dataArray[j];
+
+    if(j === size-i-1) {
+        setJ(0); 
+        setI(i+1);
+    }
+    
+    if(i<size-1 && j<size-i-1) {
+        if(swap > dataArray[j+1]) {
+            dataArray[j]=dataArray[j+1];
+            dataArray[j+1] = swap;
+            setArray(dataArray);
+            setJ(j+1); 
+        } else {
+            setJ(j+1);
+        }
+    }
+}
+
+export default BubbleSort;
